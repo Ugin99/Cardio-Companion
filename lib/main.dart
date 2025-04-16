fb8c59f5300c9c8e4e7edfb8bbc7c061af1c186e
anarbol-martinez <anarbol.martinez@martineztechnologies.com>
2025-04-15 22:14:30 -0700

    Test for new account sync

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -19,6 +19,7 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
